e9a30862f6a61c2eb0e1371ebed20edbef595531
Anshul Bheniye <81631443+anshul-bheniye@users.noreply.github.com>
2023-05-29 20:15:12 +0530

    create task object

--- src/components/ShowTask.js
+++ src/components/ShowTask.js
@@ -1,7 +1,30 @@
 
+export const ShowTask = ({addTasks, setAddTasks, editTask, setEditTask}) => {
+  const handleEdit = (id) => {
+    const searchTaskIdFromList = addTasks.find((addTask)=> addTask.id === id);
+    setEditTask(searchTaskIdFromList)
+  }
 
-export const ShowTask = () => {
+
+  const handleDelete = (id) => {
+    const keepOnEqual = addTasks.filter((addTask)=> addTask.id !== id);
+    setAddTasks(keepOnEqual);
+  }
+
   return (
-    <div>ShowTask</div>
+    <div>
+      <ul>
+        {addTasks.map((addTask)=>(
+          <li key={addTask.id}>
+            <p>
+                <span className="name">{addTask.name}</span>
+                <span className="time">{addTask.date}</span>
+            </p>
+            <i onClick={() => handleEdit(addTask.id)} className="bi bi-pencil-square"></i>
+            <i onClick={() => handleDelete(addTask.id)} className="bi bi-trash"></i>
+        </li>
+        ))}
+      </ul>
+    </div>
   )
 }
