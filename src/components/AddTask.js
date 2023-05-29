98f7aa69c11d7522b944566a913004035e215b40
Anshul Bheniye <81631443+anshul-bheniye@users.noreply.github.com>
2023-05-29 20:14:19 +0530

    update addtask

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,7 +1,40 @@
 
 
-export const AddTask = () => {
+export const AddTask = ({addTasks, setAddTasks, editTask, setEditTask}) => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const time = new Date();
+
+    if(editTask.id){
+      const checkFromList = addTasks.map((addTask)=>(
+        addTask.id === editTask.id ? {id:editTask.id, name: editTask.name, date:`${time.toLocaleDateString()} — ${time.toLocaleTimeString()}`} : addTask
+      ))
+      setAddTasks(checkFromList)
+      setEditTask({})
+    }else{
+      const makeTaskObj = {
+        id: time.getMilliseconds(),
+        name: e.target.input.value,
+        date: `${time.toLocaleDateString()} — ${time.toLocaleTimeString()}`
+      }
+      setAddTasks([...addTasks, makeTaskObj]);
+      setEditTask({})
+    }
+  }
   return (
-    <div>AddTask</div>
+    <div>
+      <form onSubmit={handleSubmit}>
+      <input 
+      type='text' 
+      name='input' 
+      placeholder='Enter the task'
+      autoComplete='off'
+      maxLength={18}
+      value={editTask.name || ''}
+      onChange={(e)=> setEditTask({...editTask, name:e.target.value})}
+      />
+      <button type="submit">ADD</button>
+      </form>
+    </div>
   )
 }
