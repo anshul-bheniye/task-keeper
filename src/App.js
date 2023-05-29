3bd2eed998d3b055d3c092d2e030ed52f93398e4
Anshul Bheniye <81631443+anshul-bheniye@users.noreply.github.com>
2023-05-29 20:14:55 +0530

    set states for accessing value

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
+import {useEffect, useState} from 'react';
 import { Header } from './components/Header';
 import {AddTask} from './components/AddTask';
 import {ShowTask} from './components/ShowTask';
 import './app.css';
 
 export const App = () => {
+  const [addTasks, setAddTasks] = useState(JSON.parse(localStorage.getItem('addTasks')) || [])
+  const [editTask, setEditTask] = useState({})
+
+  useEffect(()=>{
+    localStorage.setItem('addTasks', JSON.stringify(addTasks))
+  },[addTasks])
   return (
     <div className="app">
       <Header />
-      <AddTask />
-      <ShowTask />
+      <AddTask 
+      addTasks={addTasks}
+      setAddTasks={setAddTasks}
+      editTask={editTask}
+      setEditTask={setEditTask}
+      />
+      <ShowTask 
+      addTasks={addTasks}
+      setAddTasks={setAddTasks}
+      editTask={editTask}
+      setEditTask={setEditTask}
+      />
     </div>
   );
 }
