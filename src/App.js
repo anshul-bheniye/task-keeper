90cae38ea79047dd9b928afed5858ed47240cf3f
Anshul Bheniye <81631443+anshul-bheniye@users.noreply.github.com>
2023-05-29 17:53:47 +0530

    import header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import { Header } from './components/Header';
 import {AddTask} from './components/AddTask';
-import {ShowTask} from './components/ShowTask'
+import {ShowTask} from './components/ShowTask';
 import './app.css';
 
 export const App = () => {
   return (
-    <div className="App">
+    <div className="app">
+      <Header />
       <AddTask />
       <ShowTask />
     </div>
