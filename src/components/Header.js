5829b167a4d27d2944d40c902f84dbf391d4219d
Anshul Bheniye <81631443+anshul-bheniye@users.noreply.github.com>
2023-05-29 23:42:44 +0530

    give background changing functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,14 @@
+import {useEffect, useState} from 'react';
 import Logo from '../assets/logo.png';
 import '../app.css';
 
 export const Header = () => {
+  const[theme, setTheme] = useState(JSON.parse(localStorage.getItem('theme')) || 'med-dark');
+  useEffect(()=>{
+    document.documentElement.removeAttribute('class')
+    document.documentElement.classList.add(theme)
+    localStorage.setItem('theme', JSON.stringify(theme))
+  },[theme])
   return(
     <div className='header'>
         <div className='brand'>
@@ -9,10 +16,10 @@ export const Header = () => {
             <span>Task Keeper</span>
         </div>
         <div className='bg'>
-            <span className='neutral dot'></span>
-            <span className='dark dot'></span>
-            <span className='med-dark dot'></span>
-            <span className='gradient dot'></span>
+            <span onClick={()=>setTheme('neutral')} className={theme === 'neutral' ? 'neutral active dot': 'neutral dot'}></span>
+            <span onClick={()=>setTheme('dark')} className={theme === 'dark' ? 'dark active dot': 'dark dot'}></span>
+            <span onClick={()=>setTheme('med-dark')} className={theme === 'med-dark' ? 'med-dark active dot': 'med-dark dot'}></span>
+            <span onClick={()=>setTheme('gradient')} className={theme === 'gradient' ? 'gradient active dot': 'gradient dot'}></span>
         </div>
     </div>
   )
